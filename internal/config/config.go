0210220ae961ccbf3a06ed62565ceb4c327f4bf7
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-08-13 14:57:39 +0530

    Add configuration management and dependencies for the application

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,28 @@
 package config
 
-type HTTPServer struct{
+import (
+	"flag"
+	"os"
+)
+
+type HTTPServer struct {
 	Addr string
 }
+
+// env-default:"production"
 type Config struct {
-	Env string 
-	StoragePath string
-	HTTPServer
+	Env         string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+		configPath = *flags
+	}
 }
