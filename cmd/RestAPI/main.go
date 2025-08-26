a87b135b1fcbee9541ea4dd80792b6006c011577
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-08-26 22:17:41 +0530

    Implement student management features with SQLite storage and HTTP handlers

--- cmd/RestAPI/main.go
+++ cmd/RestAPI/main.go
@@ -1,40 +1,69 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/Harshvardhan-91/RestAPI/internal/config"
+	"github.com/Harshvardhan-91/RestAPI/internal/http/handlers/student"
+	"github.com/Harshvardhan-91/RestAPI/internal/storage/sqlite"
 )
 
 func main() {
 	// load config
 	cfg := config.MustLoad()
+	// database setup
 
-	//setup router
+	storage, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
+
+	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to RestAPI"))
-	})
+	router.HandleFunc("POST /api/students", student.New(storage))
+	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+	router.HandleFunc("GET /api/students", student.GetList(storage))
+	// setup server
 
-	//setup server
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
-	fmt.Printf("Server started %s", cfg.HTTPServer.Addr)
+
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
 	done := make(chan os.Signal, 1)
-	signal.Notify(done, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
 	go func() {
 		err := server.ListenAndServe()
 		if err != nil {
 			log.Fatal("failed to start server")
 		}
 	}()
+
 	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("server shutdown successfully")
+
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,23 +9,26 @@ import (
 )
 
 type HTTPServer struct {
-	Addr string `yaml:"addr" env:"HTTP_SERVER_ADDR" env-default:":8080"`
+	Addr string `yaml:"address" env-required:"true"`
 }
 
 // env-default:"production"
+
 type Config struct {
 	Env         string `yaml:"env" env:"ENV" env-required:"true"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 
-func MustLoad() *Config{
+func MustLoad() *Config {
 	var configPath string
+
 	configPath = os.Getenv("CONFIG_PATH")
 
 	if configPath == "" {
 		flags := flag.String("config", "", "path to the configuration file")
 		flag.Parse()
+
 		configPath = *flags
 
 		if configPath == "" {
@@ -33,15 +36,15 @@ func MustLoad() *Config{
 		}
 	}
 
-	if _, err := os.Stat(configPath); os.IsNotExist(err){
-		log.Fatalf("config file does not exist: %s", configPath);
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
 	}
+
 	var cfg Config
 
 	err := cleanenv.ReadConfig(configPath, &cfg)
-
 	if err != nil {
-		log.Fatalf("Cannot read config file: %s", err.Error())
+		log.Fatalf("can not read config file: %s", err.Error())
 	}
 
 	return &cfg
